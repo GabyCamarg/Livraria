54da2aab76db4b4b918f5324b408765a4e438207
Yasminn <carvalhosyasminn@gmail.com>
2026-06-06 02:44:10 -0300

    feat: integra menu principal a aplicacao

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import menu.MenuLivraria;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MenuLivraria menu = new MenuLivraria();
+        menu.iniciar();
+
+    }
+}
