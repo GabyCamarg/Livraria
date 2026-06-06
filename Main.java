170f4688f6338c9791bca8835d3f90962865576e
Gaby <1gabycamargo@gmail.com>
2026-06-06 05:51:39 -0300

    implementa estrutura de pedidos no sistema

--- Main.java
+++ Main.java
@@ -5,3 +5,15 @@ import model.ItemPedido;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+
+public class Main {
+
+    ArrayList<Cliente> clientes = new ArrayList<>();
+    ArrayList<Livro> livros = new ArrayList<>();
+
+    ArrayList<Pedido> pedidos = new ArrayList<>();
+    ArrayList<ItemPedido> itensPedido = new ArrayList<>();
+
+    int idPedido = 0;
+    int idItem = 0;
+}
---
+++ src/model/Pedido.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Pedido {
+
+    public int id;
+    public int idCliente;
+    public String status;
+
+}
